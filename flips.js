5ba78ca0965398990e7d3fdd42f30e33db813667
Parks Cochrane <pdcochrane@gmail.com>
2022-04-20 01:35:57 -0400

    again

--- flips.js
+++ flips.js
@@ -1,7 +1,9 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from 'minimist'
+import { createRequire } from 'module';
+let require = createRequire(import.meta.url)
 
-let args = minimist(process.args.slice(2))
+let args = require('minimist')(process.args.slice(2))
 let flips = coinFlips(args.number)
 
 console.log(flips, countFlips(flips))
