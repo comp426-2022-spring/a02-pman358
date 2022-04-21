28b89c5b169345588bbdbae989b0f8945a151fdb
Parks Cochrane <pdcochrane@gmail.com>
2022-04-20 21:27:03 -0400

    switched

--- guess-flip.js
+++ guess-flip.js
@@ -4,9 +4,9 @@ import minimist from "minimist";
 let args = minimist(process.argv.slice(2))
 let call = args['call']
 
-if(call != 'heads' || 'tails') {
-    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
-}
-else {
+if(call == 'heads' || call == 'tails') {
     console.log(flipACoin(call))
+}
+else { 
+    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
 }
